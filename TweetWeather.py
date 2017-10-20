98610143eef4e2bb046dacfec4024ea067fa9474
mbtaylor12 <mbtaylor12@hotmail.com>
2017-10-20 19:05:59 -0400

    Added currentTime to getWeather return string

--- TweetWeather.py
+++ TweetWeather.py
@@ -1,7 +1,8 @@
 from weather import Weather
+import datetime
 
 #city can be zip code or city, state
-city = ""
+city = "24141"
 #TOKEN, TOKEN_KEY, CON_SEC, CON_SEC_KEY can all be found on your Twitter Application Page
 TOKEN = ""
 TOKEN_KEY = ""
@@ -13,13 +14,15 @@ def sendTweet(message):
 
 def getWeather():
 	weather = Weather()
-
+	now = datetime.datetime.now()
 
 	location = weather.lookup_by_location(city)
 	condition = location.condition()
 	region = location.location()
 
-	return "It is currently " + condition['text'] + " and " + condition['temp'] + " Degrees Fahrenheit in " + region['city'] + "," + region['region']
+	currentTime = now.strftime("%H:%M %m/%d/%Y")
+
+	return "It is currently " + condition['text'] + " and " + condition['temp'] + " Degrees Fahrenheit in " + region['city'] + "," + region['region'] + " at " + currentTime
 
 if __name__ == '__main__':
         print getWeather()
